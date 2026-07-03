a8bbfe6e5d4c3f0c7b6e86e7eee4ec795efab248
D4v1 <davymuniz6@gmail.com>
2026-07-03 11:22:36 -0300

    setup enviroment variables

--- src/main/resources/db/migration/V3__categories.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE categories (
-                            id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-
-                            name VARCHAR(100) NOT NULL,
-                            description TEXT,
-
-                            created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-);
--- src/main/resources/db/migration/V8__fuzzy_search.sql
+++
@@ -1,3 +0,0 @@
-CREATE EXTENSION IF NOT EXISTS pg_trgm;
-CREATE INDEX idx_categories_name_trgm ON categories USING GIN (name gin_trgm_ops);
-SET pg_trgm.similarity_threshold = 0.3;
